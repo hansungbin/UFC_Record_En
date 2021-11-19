84b31899edfaad2d1e75f2b171af946a8eb64f02
hansungbin <happynewmind@gmail.com>
2021-11-20 01:24:41 +0900

    2021_1120_00

--- app/src/main/java/binny/ufc_record_en/MainActivity.kt
+++ app/src/main/java/binny/ufc_record_en/MainActivity.kt
@@ -2,10 +2,23 @@ package binny.ufc_record_en
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import binny.ufc_record_en.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        toolbar = binding.searchToolbar
+
+        setContentView(view)
+        setSupportActionBar(toolbar)
+
     }
 }
