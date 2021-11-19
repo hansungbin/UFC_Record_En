d81aad85c0ce33b65c5479e4d118e9cd84dc2051
hansungbin <happynewmind@gmail.com>
2021-11-20 02:53:37 +0900

    2021_1120_00

--- app/src/main/java/binny/ufc_record_en/MainActivity.kt
+++ app/src/main/java/binny/ufc_record_en/MainActivity.kt
@@ -3,7 +3,13 @@ package binny.ufc_record_en
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import binny.ufc_record_en.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,5 +26,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         setSupportActionBar(toolbar)
 
+        val navView: BottomNavigationView = binding.navView
+        val navController : NavController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_record, R.id.navigation_fighter,R.id.navigation_ranking
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
